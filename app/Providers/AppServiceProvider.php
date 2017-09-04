d5463b0bbecbd8491d9b8beda47eac001d744243
pt0927 <1007687752@qq.com>
2017-09-04 16:32:51 +0800

    菜单栏动态加载

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        view()->composer('Public.menu',function($view){
+            $menuary =array(
+                '系统管理'=>array('icon'=>'xe62d;','submenu'=>array('user'=>'管理员设置','webinfo'=>'网站信息','logininfo'=>'登录信息管理','r_access'=>'注册及访问控制','node'=>'节点管理')),
+                '用户中心'=>array('icon'=>'xe62d;','submenu'=>array('userlist'=>'用户列表','useradd'=>'用户增加')),
+                '众筹管理'=>array('icon'=>'xe62d;','submenu'=>array('zctype'=>'众筹类别','zclist'=>'众筹列表','zcadd'=>'增加众筹项目')),
+                '订单管理'=>array('icon'=>'xe62d;','submenu'=>array('orderlist'=>'订单列表')),
+                '第三方信息管理'=>array('icon'=>'xe62d;','submenu'=>array('Interface'=>'接口列表','addInterface'=>'增加接口')),
+                '前端设置'=>array('icon'=>'xe62d;','submenu'=>array('link'=>'友情链接','img'=>'图片管理')),
+            );
+            $view->with('menu',$menuary);
+        });
     }
 
     /**
